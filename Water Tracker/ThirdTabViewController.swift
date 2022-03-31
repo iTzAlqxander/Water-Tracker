597623eacd7231c81812df3d99dda160c1263ad8
Alexander Masztak <amasztak3281@stu.d214.org>
2022-03-31 14:53:54 -0500

    added cool slider for custom add

--- Water Tracker/ThirdTabViewController.swift
+++ Water Tracker/ThirdTabViewController.swift
@@ -8,15 +8,46 @@
 import UIKit
 
 class ThirdTabViewController: UIViewController {
-
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var ouncesLabel: UILabel!
+    @IBOutlet weak var displayLabel: UILabel!
     @IBOutlet weak var slider: UISlider!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+    
         slider.transform = CGAffineTransform(rotationAngle: .pi / -2)
-                
+        
+  
+        progressBar.transform = CGAffineTransform(rotationAngle: .pi / -2)
+        progressBar.transform = progressBar.transform.scaledBy(x: 2.96, y: 80)
+        progressBar.layer.cornerRadius = 0
+        progressBar.layer.sublayers![1].cornerRadius = 0
+    }
+   
+        
+    @IBAction func sliderSlid(_ sender: UISlider) {
+        
+        let n = Int(slider.value) % 101
+        let m = n * 8
+        displayLabel.text = "\(n)" + " C."
+        ouncesLabel.text = "\(m)" + " Oz."
+        
+        let b = Int(slider.value) % 101
+        let c = Double(b)
+        let d = c/100
+        //print(d)
+        let x = Double(b) * 3.5
+        //print(x)
+        let y = 574 - x
+        let z = 608 - x
+        
+        progressBar.progress = Float(d)
+        displayLabel.frame.origin = CGPoint (x:96, y:y)
+        ouncesLabel.frame.origin = CGPoint (x:96, y:z)
     }
     
-
 
 }
